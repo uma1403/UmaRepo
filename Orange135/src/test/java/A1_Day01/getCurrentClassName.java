cbb92bf3a7c052fd17cd8cb0b59006f72e8c053a
Uma <Uma@LAPTOP-I8106HAO>
2021-08-13 12:24:22 +0530

    new lines has been add

--- Orange135/src/test/java/A1_Day01/getCurrentClassName.java
+++ Orange135/src/test/java/A1_Day01/getCurrentClassName.java
@@ -11,9 +11,11 @@ public class getCurrentClassName {
 		
 
 		 System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		 
 	      // WebDriver driver=new ChromeDriver();
 	       //driver.manage().window().maximize();
           // driver.get("https://www.google.com/");
+		 
 		 getCurrentClassName obj=new getCurrentClassName();
 		  obj.className();
 	}
