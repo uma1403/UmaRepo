646ba7cefd54c721d0708584f6332f655918410a
Uma <Uma@LAPTOP-I8106HAO>
2021-08-13 12:26:30 +0530

    few comments removed

--- Orange135/src/test/java/A1_Day01/getCurrentClassName.java
+++ Orange135/src/test/java/A1_Day01/getCurrentClassName.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class getCurrentClassName {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-
 		 System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 		 
 	      // WebDriver driver=new ChromeDriver();
